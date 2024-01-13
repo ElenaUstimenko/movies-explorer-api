4e281adc28b73d9007799a3ea29e63339b54cce5
Elena Ustimenko <ystimenko.e@mail.ru>
2024-01-13 21:24:51 +0300

    id

--- models/movie.js
+++ models/movie.js
@@ -59,6 +59,10 @@ const movieSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  id: {
+    type: Number,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('movie', movieSchema);
